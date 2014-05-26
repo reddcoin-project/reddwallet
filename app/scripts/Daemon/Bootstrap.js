b5f33e3392b2d68732e8cf4a017586f0d787d172
Sam Vaughton <samvaughton@gmail.com>
2014-05-26 21:10:21 +0100

    Changed the daemon on linux to 32bit for better compatibility.

--- app/scripts/Daemon/Bootstrap.js
+++ app/scripts/Daemon/Bootstrap.js
@@ -40,7 +40,7 @@ App.Daemon.Bootstrap = (function () {
         this.daemonMap = {
             'linux': {
                 'x32': 'daemons/reddcoind-linux-32',
-                'x64': 'daemons/reddcoind-linux-64',
+                'x64': 'daemons/reddcoind-linux-32',
                 'default': 'daemons/reddcoind-linux-32'
             },
             'win32': {
