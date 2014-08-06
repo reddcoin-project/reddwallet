786a4e91c556d6305e79f15b07de253091206c47
Sam Vaughton <samvaughton@gmail.com>
2014-08-06 14:54:46 +0100

    Updated the grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,12 @@ module.exports = function (grunt) {
             options: {
                 version: "0.10.0",
                 buildDir: './dist',
-                platforms: ['win', 'osx', 'linux64', 'linux32']
+                platforms: ['win', 'osx', 'linux64', 'linux32'],
+                appName: "ReddWallet",
+                appVersion: "1.0.0",
+                buildType: "versioned",
+                winIco: "reddcoin-favi.ico",
+                macIcns: "reddcoin_logo.icns"
             },
             src: './_public/**/*'
         }
