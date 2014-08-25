09baa8e51de816fbbbc881a6021ae2031461dee6
Sam Vaughton <samvaughton@gmail.com>
2014-08-25 13:22:52 +0100

    Moved the functions out of the filter file.

--- app/scripts/Utils/functions.js
+++ app/scripts/Utils/functions.js
@@ -30,3 +30,40 @@ String.prototype.toUpperFirst = function() {
 String.prototype.toLowerFirst = function() {
     return this.substring(0, 1).toLowerCase() + this.substring(1);
 };
+
+
+function smartTrim(string, maxLength) {
+
+    if (!string) return string;
+    if (maxLength < 1) return string;
+    if (string.length <= maxLength) return string;
+    if (maxLength == 1) return string.substring(0, 1) + '...';
+
+
+    var midpoint = Math.ceil(string.length / 2);
+    var toRemove = string.length - maxLength;
+    var lStrip = Math.ceil(toRemove / 2);
+    var rStrip = toRemove - lStrip;
+
+    return string.substring(0, midpoint - lStrip) + '...' + string.substring(midpoint + rStrip);
+}
+
+/* A function that splits a string `limit` times and adds the remainder as a final array index.
+ * > var a = 'convoluted.madeup.example';
+ * > a.split('.', 1);
+ * < ['convoluted']
+ * // What I expected:
+ * < ['convoluted', 'madeup.example']
+ */
+function properSplit(str, separator, limit) {
+    str = str.split(separator);
+
+    if(str.length > limit) {
+        var ret = str.splice(0, limit);
+        ret.push(str.join(separator));
+
+        return ret;
+    }
+
+    return str;
+}
--- app/scripts/filters.js
+++ app/scripts/filters.js
@@ -8,42 +8,6 @@ angular.module('app.filters', []).filter('interpolate', [
     }
 ]);
 
-function smartTrim(string, maxLength) {
-
-    if (!string) return string;
-    if (maxLength < 1) return string;
-    if (string.length <= maxLength) return string;
-    if (maxLength == 1) return string.substring(0, 1) + '...';
-
-
-    var midpoint = Math.ceil(string.length / 2);
-    var toRemove = string.length - maxLength;
-    var lStrip = Math.ceil(toRemove / 2);
-    var rStrip = toRemove - lStrip;
-
-    return string.substring(0, midpoint - lStrip) + '...' + string.substring(midpoint + rStrip);
-}
-
-/* A function that splits a string `limit` times and adds the remainder as a final array index.
- * > var a = 'convoluted.madeup.example';
- * > a.split('.', 1);
- * < ['convoluted']
- * // What I expected:
- * < ['convoluted', 'madeup.example']
- */
-function properSplit(str, separator, limit) {
-    str = str.split(separator);
-
-    if(str.length > limit) {
-        var ret = str.splice(0, limit);
-        ret.push(str.join(separator));
-
-        return ret;
-    }
-
-    return str;
-}
-
 angular.module('app.filters').filter('to_trusted', ['$sce', function($sce) {
     return function(text) {
         return $sce.trustAsHtml(text);
